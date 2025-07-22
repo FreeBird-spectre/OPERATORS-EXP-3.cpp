e2f74807e2d6e4ad2f1d6fd90649e1e44996fd21
FreeBird-spectre <sethabir699@gmail.com>
2025-07-22 19:17:53 +0530

    Update Positive or negative.cpp

--- Positive or negative.cpp
+++ Positive or negative.cpp
@@ -1,1 +1,29 @@
+// Abir Seth
+//PRN-24070123003
+// EnTC A1
+// EXP 3A
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    int number;
+    cout<<"enter a number:";
+    cin>>number;
+   if (number>0) {
+       cout << "the number is positive."<< endl; }
+       
+   else if (number < 0) {
+       cout << "the number is negative."<< endl; }
+           
+   else {
+     cout<< "the number is zero. " << endl; }
+               
+    return 0;
+}
+
+enter a number:7
+the number is positive.
+=== Code Execution Successful ===
 
